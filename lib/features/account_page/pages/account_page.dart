48b44ff9a18d537412bb2d8496fd6aacee380a42
Darya Kovalchuk <daryakovalchuk167@gmail.com>
2024-07-01 01:35:56 +0300

    feat: add account page ui and all transactions page ui

--- lib/features/account_page/pages/account_page.dart
+++ lib/features/account_page/pages/account_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:modsen_banking/features/account_page/widgets/transaction_card.dart';
 
 class AccountPage extends StatelessWidget {
   const AccountPage({
@@ -7,10 +8,104 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
-      ),
-    );
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: const Color.fromARGB(255, 1, 1, 1),
+          padding: EdgeInsets.only(
+            left: MediaQuery.of(context).size.width * 0.042,
+            right: MediaQuery.of(context).size.width * 0.042,
+            top: MediaQuery.of(context).size.height * 0.016,),
+          child: Column(
+            children: [
+              const Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'Account',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontSize: 28,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.011,
+                  bottom: MediaQuery.of(context).size.height * 0.03),
+                height: 92,
+                color: const Color.fromARGB(255, 28, 28, 30),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Recent Transactions',
+                    style: TextStyle(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontSize: 28,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  ),
+                  GestureDetector(
+                    child: const Text(
+                      'VIEW ALL',
+                      style: TextStyle(
+                      color: Color.fromARGB(255, 64, 156, 255),
+                      fontSize: 13,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 16),
+              Expanded(
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  child: ListView(
+                    children: [
+                      TransactionCard(
+                      'Hello world',
+                      '30.06.2024',
+                      '15.0',
+                      'Declined'
+                    ),
+                      TransactionCard(
+                      'Modsen',
+                      '30.05.2024',
+                      '25.0',
+                      'In progress'
+                    ),
+                      TransactionCard(
+                      'Gift',
+                      '25.06.2024',
+                      '100.0',
+                      'Executed'
+                    )
+                    ]
+                  ),
+                ),
+              ),
+            ],
+          )
+          ),
+        ),
+          floatingActionButton: Positioned(
+          bottom: 16.0,
+          left: 16.0,
+          child: FloatingActionButton(
+            backgroundColor: const Color.fromARGB(255, 64, 156, 255),
+            onPressed: () {
+            },
+            shape: const CircleBorder(),
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      );
   }
 }
+  
